267ed415711ab7c40c4f22bec65303ad669cb6b6
= <bruno@paschoalinoto.com>
2016-01-21 16:17:52 -0200

    it looks even nicer

--- public/game.js
+++ public/game.js
@@ -30,6 +30,8 @@ if (localStorage["connect4-name"] == "" || localStorage["connect4-name"] == unde
 }
 socket.emit("join", name);
 
+var globalwrapper = document.getElementById("globalchat-wrapper");
+var roomwrapper = document.getElementById("roomchat-wrapper");
 var globalarea = document.getElementById("global-chat");
 var globalinput = document.getElementById("globalinput")
 function globalchat(string) {
@@ -64,10 +66,12 @@ function makeroom() {
     socket.emit("makeroom", roomname);
 }
 
+var amcreator = false;
 var mkroom = document.getElementById("mkroom");
 socket.on("onroom", function(id) {
     mkroom.style.display = "none";
     socket.emit("joinroom", id);
+    amcreator = true;
 });
 
 var roomlist = document.getElementById("roomlist");
@@ -88,38 +92,6 @@ socket.on("canMakeRoom", function() {
     mkroom.style.display = "inline-block";
 });
 
-var yours = document.getElementById("yourwins");
-var theirs = document.getElementById("theirwins");
-var stales = document.getElementById("stalemates");
-var gameout = document.getElementById("game-out");
-
-function drawGame(board) {
-    gameout.innerHTML = "";
-    for (row_index in board) {
-        var row = board[row_index];
-        for (col_index in row) {
-            var but = document.createElement("span");
-            var cell = row[col_index];
-            switch (cell) {
-                case 0: but.setAttribute("class", "empty"); break;
-                case 1: but.setAttribute("class", "creator"); break;
-                case 2: but.setAttribute("class", "opponent"); break;
-            }
-            var col = parseInt(col_index) + 1;
-            but.setAttribute("onclick", "socket.emit(\"playAt\", " + col + ")");
-            gameout.appendChild(but);
-        }
-        gameout.innerHTML += "<div style=\"height: 24px;\">&nbsp;</span><br>"
-    }
-}
-
-socket.on("game-status", function (status) {
-    yours.innerHTML = status.yourwins;
-    theirs.innerHTML = status.theirwins;
-    stales.innerHTML = status.stalemates;
-    drawGame(status.board);
-});
-
 socket.on("couldntjoin", function(err) {
     alert("Failed to join room: " + err);
 });
@@ -129,12 +101,23 @@ var withroom = document.getElementById("withroom");
 var roomname = document.getElementById("room_name");
 var opponentname = document.getElementById("opponent_name");
 var roomarea = document.getElementById("room-chat");
-var roomlinput = document.getElementById("roominput")
+var roomlinput = document.getElementById("roominput");
+var youcolor = document.getElementById("youcolor");
+var themcolor = document.getElementById("themcolor");
 socket.on("joinsuccess", function(room) {
     noroom.style.display = "none";
-    withroom.style.display = "inline-block";
+    withroom.style.display = "block";
+    globalwrapper.style.display = "none";
+    roomwrapper.style.display = "block";
     roomname.innerHTML = room.name;
     roomarea.innerHTML = "Room chat for \"" + room.name + "\"";
+    if (amcreator) {
+        youColor.style.backgroundColor = "yellow";
+        themColor.style.backgroundColor = "red";
+    } else {
+        youColor.style.backgroundColor = "red";
+        themColor.style.backgroundColor = "yellow";
+    }
 });
 
 socket.on("opponent", function(name) {
@@ -161,10 +144,74 @@ socket.on("user-room-message", function(message) {
     roomchat(message.sender + ": " + message.text);
 });
 
+var yours = document.getElementById("yourwins");
+var theirs = document.getElementById("theirwins");
+var stales = document.getElementById("stalemates");
+var gameout = document.getElementById("game-out");
+var turn = document.getElementById("turn");
+var drawnRows, drawCols;
+
+function drawGame(board) {
+    gameout.innerHTML = "";
+    drawnRows = board.length;
+    drawnCols = board[0].length;
+    for (row_index in board) {
+        var row = board[row_index];
+        for (col_index in row) {
+            var but = document.createElement("span");
+            var cell = row[col_index];
+            switch (cell) {
+                case 0: but.setAttribute("class", "empty"); break;
+                case 1: but.setAttribute("class", "creator"); break;
+                case 2: but.setAttribute("class", "opponent"); break;
+            }
+            var col = parseInt(col_index) + 1;
+            but.setAttribute("onclick", "socket.emit(\"playAt\", " + col + ")");
+            but.setAttribute("id", "cell-" + row_index + "-" + col_index);
+            but.setAttribute("onmouseover", "boldColumn(" + col_index + ")");
+            but.setAttribute("onmouseout", "unboldColumn(" + col_index + ")");
+            gameout.appendChild(but);
+        }
+        gameout.innerHTML += "<div style=\"height: 44px;\">&nbsp;</span><br>"
+    }
+}
+
+function boldColumn(column) {
+    if (!yourturn) return;
+    for (var row = 0; row < drawnRows; row++) {
+        var elem = document.getElementById("cell-" + row + "-" + column);
+        elem.style.border = "1px solid black";
+    }
+}
+
+function unboldColumn(column) {
+    for (var row = 0; row < drawnRows; row++) {
+        var elem = document.getElementById("cell-" + row + "-" + column);
+        elem.style.border = "1px solid gray";
+    }
+}
+
+var yourturn = false;
+socket.on("game-status", function (status) {
+    yours.innerHTML = status.yourwins;
+    theirs.innerHTML = status.theirwins;
+    stales.innerHTML = status.stalemates;
+    drawGame(status.board);
+    yourturn = status.yourturn;
+    if (status.yourturn) {
+        turn.innerHTML = "<b>Your turn</b>";
+    } else {
+        turn.innerHTML = "Their turn";
+    }
+});
+
 socket.on("destroyed", function(message) {
     alert("The room you were in was removed. Reason:\n" + message);
-    noroom.style.display = "inline-block";
+    noroom.style.display = "block";
     withroom.style.display = "none";
     roomarea.innerHTML = "";
     roominput.value = "";
+    globalwrapper.style.display = "block";
+    roomwrapper.style.display = "none";
+    amcreator = false;
 });
